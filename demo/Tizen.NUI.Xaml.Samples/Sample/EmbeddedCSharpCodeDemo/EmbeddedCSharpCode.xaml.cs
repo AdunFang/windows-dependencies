7b1f84f21d20495cfe37e81ceb9ebb4b91c900c6
Fang Xiaohui <xiaohui fang>
2020-08-04 09:07:29 +0800

    [NUI]Add demo of NUI Application

---
+++ demo/Tizen.NUI.Xaml.Samples/Sample/EmbeddedCSharpCodeDemo/EmbeddedCSharpCode.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tizen.NUI.Examples
+{
+    public partial class EmbeddedCSharpCode
+    {
+        public EmbeddedCSharpCode()
+        {
+            InitializeComponent();
+        }
+    }
+}
